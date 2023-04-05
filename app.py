30e8c6491fe9a0ecdf4258e8f838f7bca1eab3cc
KellyPared <kellschu333@gmail.com>
2023-04-05 13:20:54 +0200

    final

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect, jsonify
 import json
 
 
-
 app = Flask(__name__)
 
 # Initialize PyMongo to work with MongoDBs this is only valid on Negin's PC since she has the Mongo_db 
@@ -33,20 +32,18 @@ app = Flask(__name__)
 def index():
     with open('data/brewery_untappd_export_MD.json', 'r', encoding="utf8") as myfile:
         data_untappd = myfile.read()
-        data_untappd= json.loads(data_untappd)
+        data_untappd = json.loads(data_untappd)
     return render_template("index.html", data_untappd=data_untappd)
 
-@app.route("/about.html")
-def about_us():
-    return render_template("about.html")
+@app.route("/api/brewery_untappd_export_MD.json")
+def datajson():
+    #connect to Mongodb databse
+    with open('data/brewery_untappd_export_MD.json', 'r', encoding="utf8") as myfile:
+        data_untappd = myfile.read()
+        data_untappd= json.loads(data_untappd)
 
-@app.route("/faq.html")
-def faq():
-    return render_template("faq.html")
+    return jsonify(data_untappd)
 
-@app.route("/bar_chartjs_lib.html")
-def bar_chart():
-    return render_template("bar_chartjs_lib.html")
 
 @app.route("/api/brewery_api_export_MD.json")
 def dataapijson():
@@ -64,6 +61,21 @@ def datageojson():
         data_api= json.loads(data_api)
 
     return jsonify(data_api)
+
+
+@app.route("/about.html")
+def about_us():
+    return render_template("about.html")
+
+
+@app.route("/faq.html")
+def faq():
+    return render_template("faq.html")
+
+@app.route("/bar_chartjs_lib.html")
+def bar_chart():
+    return render_template("bar_chartjs_lib.html")
+
 
 @app.route("/breweries.html")
 def breweries():
